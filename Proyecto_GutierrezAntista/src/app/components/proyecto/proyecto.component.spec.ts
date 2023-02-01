5afc4ef3d5f48f62969a797202af4183f80af94f
anah028 <116830196+anah028@users.noreply.github.com>
2023-02-01 14:58:03 -0300

    bootstrap en header y saque barra horizontal. se termino estructura

--- Proyecto_GutierrezAntista/src/app/components/proyecto/proyecto.component.spec.ts
+++ Proyecto_GutierrezAntista/src/app/components/proyecto/proyecto.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import { ProyectoComponent } from './proyecto.component';
 
-describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
+describe('ProyectoComponent', () => {
+  let component: ProyectoComponent;
+  let fixture: ComponentFixture<ProyectoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ ProyectoComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(ProyectoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -21,4 +21,3 @@ describe('HeaderComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-
