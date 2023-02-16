fe54dc0530ca41bcdb64ddf9fd5ba811ed828bce
anah028 <116830196+anah028@users.noreply.github.com>
2023-02-16 20:32:09 -0300

    armando backend2

--- Proyecto_GutierrezAntista/BackEnd/src/main/java/com/portfolio/mgb/Interface/IPersonaService.java
+++ Proyecto_GutierrezAntista/BackEnd/src/main/java/com/portfolio/mgb/Interface/IPersonaService.java
@@ -4,10 +4,24 @@
  */
 package com.portfolio.mgb.Interface;
 
+import com.portfolio.mgb.Entity.Persona;
+import java.util.List;
+
 /**
  *
  * @author anilu
  */
 public interface IPersonaService {
+    //traer una personas//
+    public List <Persona> getPersona();
+    
+    //Guardar un objeto de tipo persona//
+    public void savePersona(Persona persona);
+    
+    //Eliminar un objeto pero lo buscamos por ID//
+    public void deletePersona(Long id);
+    
+    //Buscar una persona por id//
+    public Persona findPersona(Long id);
     
 }
