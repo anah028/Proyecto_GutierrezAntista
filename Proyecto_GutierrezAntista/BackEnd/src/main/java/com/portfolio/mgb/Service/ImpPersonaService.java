463c04f9923993bf786ee374f2786bffc5a6687d
anah028 <116830196+anah028@users.noreply.github.com>
2023-02-20 15:51:57 -0300

    backend

--- Proyecto_GutierrezAntista/BackEnd/src/main/java/com/portfolio/mgb/Service/ImpPersonaService.java
+++ Proyecto_GutierrezAntista/BackEnd/src/main/java/com/portfolio/mgb/Service/ImpPersonaService.java
@@ -6,7 +6,9 @@ package com.portfolio.mgb.Service;
 
 import com.portfolio.mgb.Entity.Persona;
 import com.portfolio.mgb.Interface.IPersonaService;
+import com.portfolio.mgb.Repository.IPersonaRepository;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
@@ -17,25 +19,27 @@ import org.springframework.stereotype.Service;
  * @author anilu
  */
 public class ImpPersonaService implements IPersonaService{
-
+    @Autowired IPersonaRepository ipersonaRepository;
     @Override
     public List<Persona> getPersona() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        List<Persona> persona = ipersonaRepository.findAll();
+   return persona;
     }
 
     @Override
     public void savePersona(Persona persona) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       ipersonaRepository.save(persona);
     }
 
     @Override
     public void deletePersona(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       ipersonaRepository.deleteById(id);
     }
 
     @Override
     public Persona findPersona(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Persona persona = ipersonaRepository.findById(id) .orElse(null);
+        return persona;
     }
     
 }
